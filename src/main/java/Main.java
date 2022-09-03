cabf4f179936d16165d4f26957de285179e781be
Admin <nequ@hotmail.se>
2022-09-03 17:35:37 +0200

    Input checker skapad, press enter skapad, base switchen klar saknar bara detaljer.
    Operationns skapade, samt finslip så de fungerar med de kommande testen

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.util.List;
+
 import java.util.Scanner;
 
 public class Main {
@@ -6,26 +6,48 @@ public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
 
-    public static void main(String[] args) {
-
-
+    public static void main(String[] args)
+    {
         boolean inuse = true;
-        while (inuse) {
+        while (inuse)
+        {
             System.out.println("Welcome to Calculator 3000!");
             System.out.println("Do you want to:\n(1) Add\n(2) Subtract");
             System.out.println("(3) Multiply\n(4) Divide\n(5) Square root\n(6) Exit");
 
             inputChecker();
-            int input = scanner.nextInt();
 
-            int choice = input;
-            switch (choice) {
+            int choice = scanner.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    handleAddition();
+                    break;
+
+                case 2:
+                    handleSubtraction();
+                    break;
+
+                case 3:
+                    handleMultiplication();
+                    break;
+
                 case 4:
                     handleDivision();
                     break;
+
+                case 5:
+                    handleSqrt();
+                    break;
+
                 case 6:
                     inuse = false;
                     System.out.println("You've selected to quit the calculator. Goodbye!");
+                    break;
+
+                default:
+                    System.out.println("Invalid input, try again");
+                    pressEnterToContinue();
                     break;
             }
         }
@@ -33,35 +55,84 @@ public class Main {
     }
 
 
-    public static void handleAddition() {
-
+    public static void handleAddition()
+    {
+        System.out.println("You've opted for addition");
+        System.out.println("Enter your first term: ");
+        inputChecker();
+        double term1 = scanner.nextDouble();
+        System.out.println("Enter your second term: ");
+        inputChecker();
+        double term2 = scanner.nextDouble();
+        double sum = Operations.add(term1, term2);
+        System.out.println("The result of " + term1 + " plus " + term2 + " is " + sum);
+        pressEnterToContinue();
     }
 
+    public static void handleSubtraction()
+    {
+        System.out.println("You've opted for subtraction");
+        System.out.println("Enter your first term: ");
+        inputChecker();
+        double term1 = scanner.nextDouble();
+        System.out.println("Enter your second term: ");
+        inputChecker();
+        double term2 = scanner.nextDouble();
+        double difference = Operations.subtract(term1, term2);
+        System.out.println("The result of " + term1 + " minus " + term2 + " is " + difference);
+        pressEnterToContinue();
+    }
 
-    public static void handleDivision() {
+    public static void handleMultiplication()
+    {
+        System.out.println("You've opted for multiplication");
+        System.out.println("Enter your first factor: ");
+        inputChecker();
+        double factor1 = scanner.nextDouble();
+        System.out.println("Enter your second factor: ");
+        inputChecker();
+        double factor2 = scanner.nextDouble();
+        double product = Operations.multiply(factor1, factor2);
+        System.out.println("The result of " + factor1 + " times " + factor2 + " is " + product);
+        pressEnterToContinue();
+    }
+    public static void handleDivision()
+    {
         System.out.println("You've opted for division");
         System.out.println("Enter your numerator: ");
         inputChecker();
-        int numerator = scanner.nextInt();
+        double numerator = scanner.nextDouble();
         System.out.println("Enter your denominator: ");
         inputChecker();
-        int denominator = scanner.nextInt();
+        double denominator = scanner.nextDouble();
         double quotient = Operations.divide(numerator, denominator);
         System.out.println("The result of " + numerator + " divided by " + denominator + " is " + quotient);
         pressEnterToContinue();
     }
 
-
-    private static void pressEnterToContinue() {
-        System.out.println("Press Enter key to continue");
-        try {
+    public static void handleSqrt()
+    {
+        System.out.println("You've opted for square root");
+        System.out.println("Enter your radicand: ");
+        inputChecker();
+        double radicand = scanner.nextDouble();
+        double result = Operations.sqrt(radicand);
+        System.out.println("The result of " + radicand + " squared " + " is " + result);
+        pressEnterToContinue();
+    }
+    private static void pressEnterToContinue()
+    {
+        System.out.println("Press Enter to continue");
+        try
+        {
             System.in.read();
             scanner.nextLine();
-        } catch (Exception e) {
         }
+        catch (Exception e) {}
     }
+
     private static void inputChecker(){
-        while (!scanner.hasNextInt()) {
+        while (!scanner.hasNextDouble()) {
             System.out.println("Please write a number");
             scanner.next();
         }
--- src/main/java/Operations.java
+++ src/main/java/Operations.java
@@ -1,9 +1,30 @@
 public class Operations {
 
-    public static double divide(int numerator, int denominator){
-        double result = numerator / denominator;
-        return result;
-    };
+    public static double divide(double numerator, double denominator)
+    {
+        return numerator / denominator;
+    }
 
+    
+
+    public static double add(Double term1, Double term2)
+    {
+        return term1 + term2;
+    }
+
+    public static double subtract(double term1, double term2)
+    {
+        return term1 - term2;
+    }
+
+
+    public static double multiply(double factor1, double factor2)
+    {
+        return factor1 * factor2;
+    }
+
+    public static double sqrt(double radicand)
+    {
+        return Math.sqrt(radicand);
+    }
 }
-
